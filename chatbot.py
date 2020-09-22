02c09662e2586cfee9cb489fdd082d9a94f66f2f
nikhilbachani <nikhilbachani25@gmail.com>
2020-09-22 16:51:14 -0400

    Added code to count

--- chatbot.py
+++ chatbot.py
@@ -15,4 +15,14 @@ remainder5 = int(input())
 remainder7 = int(input())
 
 age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
-print("Your age is " , age, "; that's a good time to start programming!")
+print("Your age is " + str(age) + "; that's a good time to start programming!")
+
+# Stage 4: Let's count!
+print("Now I will prove to you that I can count to any number you want.")
+number = int(input())
+start = 0
+while start <= number:
+	print(start, "!")
+	start += 1
+
+print("Completed, have a nice day!")
